d7bd28bd03c0c311a61332451eb2f188380d74d8
Your Name <you@example.com>
2023-10-10 19:08:34 +0300

    Adding basic test project

---
+++ Summator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+           int result = Summator.Sum(new int[] { 1, 2, 3, 4, 5, 6});
+            if (result == 21)
+            {
+                Console.WriteLine("Test passed!");
+            }
+            else {
+                Console.WriteLine("Test failed!");
+            }
+        }
+    }
+}
---
+++ Summator/Summator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summator
+{
+    public static class Summator
+    {
+        public static int Sum(int[] arr) {
+           
+            
+            int sum = 0;
+
+            for (int i = 1; i < arr.Length ; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+    }
+}
