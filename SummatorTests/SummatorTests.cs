b214719b2345b6f5b86c503c9ea45f4fc8bc9954
Your Name <you@example.com>
2023-10-11 13:14:35 +0300

    Added test folder again, but on a seperate branch

---
+++ SummatorTests/SummatorTests.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+
+namespace Summator.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test_Sum_TwoPositiveNums() {
+
+            int result = Summator.Sum(new int[] {5, 7});
+
+            Assert.That(result == 12);
+        }
+
+        [Test]
+        public void Test_Sum_OnePositiveNum()
+        {
+
+            int result = Summator.Sum(new int[] { 5});
+
+            Assert.That(result == 5);
+        }
+
+        [Test]
+        public void Test_Sum_EmptyArray() {
+            int expected = Summator.Sum(new int[] { });
+            Assert.That(expected == 0);
+        }
+        [Test]
+        public void Test_Sum_EmptyArra()
+        {
+            int expected = Summator.Sum(new int[] { });
+            Assert.That(expected == 0);
+        }
+    }
+
+}
